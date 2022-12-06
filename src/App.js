20d9a50e6f5451c5d61e9dd1d405a5a77e4e8ad9
Nikolaus Hruska <nhruska@users.noreply.github.com>
2022-12-06 14:32:39 +0000

    updated welcome message from browser VS Code

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            App URL
+            nhruska@alleni.com
           </a>
         </p>
       </header>
